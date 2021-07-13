0b49c2328328690741c339bf2d2545fcb86b7583
FRANCISCO OLIVEIRA <fransoliveira@hotmail.com>
2021-07-13 14:29:50 -0300

    ClienteRepository

---
+++ OlifransHealthCareERP.Data/Repository/ClienteRepository.cs
@@ -0,0 +1,26 @@
+﻿using OlifransHealthCareERP.Core.Domain;
+using OlifransHealthCareERP.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlifransHealthCareERP.Data.Repository
+{
+    public class ClienteRepository  //Reponsavel pelo acesso aos dados
+    {
+        private ContextDBOlifransHealthCare context;
+
+        public ClienteRepository(ContextDBOlifransHealthCare context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<Cliente>> GetClientesAsync()
+        {
+
+        }
+
+    }
+}
