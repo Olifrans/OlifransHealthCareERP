1cf273ffb644ecc9eb1274890345dba9cb043ddb
FRANCISCO OLIVEIRA <fransoliveira@hotmail.com>
2021-07-13 15:22:49 -0300

    IClienteRepository

--- OlifransHealthCareERP.Data/Repository/ClienteRepository.cs
+++ OlifransHealthCareERP.Data/Repository/ClienteRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OlifransHealthCareERP.Core.Domain;
 using OlifransHealthCareERP.Data.Context;
+using OlifransHealthCareERP.Manager.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace OlifransHealthCareERP.Data.Repository
 {
-    public class ClienteRepository : IClienteRepository  //Reponsavel pelo acesso aos dados
+    public class ClienteRepository : IClienteRepository  //Reponsavél pelo acesso aos dados
     {
         private ContextDBOlifransHealthCare _context;
 
@@ -20,14 +21,12 @@ namespace OlifransHealthCareERP.Data.Repository
 
         public async Task<IEnumerable<Cliente>> GetClientesAsync()
         {
-            return await _context.Clientes.AsNoTracking().ToListAsync(); //Retorna todos os clientes
+            return await _context.Clientes.AsNoTracking().ToListAsync();
         }
 
         public async Task<Cliente> GetClientesAsync(int id)
         {           
             return await _context.Clientes.FindAsync(id);
-        }
-
-
+        }       
     }
 }
--- OlifransHealthCareERP.Manager/Interfaces/IClienteRepository.cs
+++ OlifransHealthCareERP.Manager/Interfaces/IClienteRepository.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OlifransHealthCareERP.Core.Domain;
 
 namespace OlifransHealthCareERP.Manager.Interfaces
 {
-    class IClienteRepository  //Diz sobre as regras de negócio
+    public interface IClienteRepository  //Regras de negócios
     {
+        Task<IEnumerable<Cliente>> GetClientesAsync();
+        Task<Cliente> GetClientesAsync(int id);
     }
 }
